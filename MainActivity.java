9a1c09b27fb78ed9d0bffc89b3d7f96c66e7a4de
arhahrear30 <arshahrear30@gmail.com>
2025-05-21 03:11:24 +0600

    Update button properly add  finally MainActivity.java
    আগেরটায় data input এর সময় update এর url এ অনেকগুলোয় = চিহ্ন দিতে ভুলে গেছিলাম

--- MainActivity.java
+++ MainActivity.java
@@ -188,12 +188,13 @@ public class MainActivity extends AppCompatActivity {
             String email = hashMap.get("email");
 
             tvId.setText(id);
-            tvEmail.setText(email);
-            tvMobile.setText(mobile);
             tvName.setText(name);
+            tvMobile.setText(mobile);
+            tvEmail.setText(email);
+
 //;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-            //লেখা input করে update এ click করলে যেটার update এ click করছি ঐটা update হয়ে যাবে 
+            //লেখা input করে update এ click করলে যেটার update এ click করছি ঐটা update হয়ে যাবে
             buttonUpdate.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
@@ -206,14 +207,14 @@ public class MainActivity extends AppCompatActivity {
                     //"&m=" +mobile+
                     //"&e=" +email ;
                     progressBar.setVisibility(View.VISIBLE);
-                    String url = "https://nubsoft.xyz/data.php?id="+id+"&name"+name+"&mobile"+mobile+"&email"+email ;
+                    String url = "https://nubsoft.xyz/update.php?id="+id+"&name="+name+"&mobile="+mobile+"&email="+email ;
                     StringRequest request = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
                         @Override
                         public void onResponse(String response) {
                             progressBar.setVisibility(View.GONE);
                             new AlertDialog.Builder(MainActivity.this)
                                     .setTitle("Server Response")
-                                    .setMessage("Response")
+                                    .setMessage(response)
                                     .show();
                             loadData();
                         }
